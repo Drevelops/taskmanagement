60f2b8be72031e6fe83ca9428f8b78288379af90
awheel11 <awheel11@wgu.edu>
2025-02-02 02:21:01 -0700

    Initial project structure

--- user-service/src/main/java/com/taskmanagement/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/taskmanagement/userservice/UserserviceApplication.java
@@ -2,12 +2,22 @@ package com.taskmanagement.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@RestController
+	public class HealthController {
+		@GetMapping("/health")
+		public String health() {
+			return "Service is healthy!";
+		}
 	}
 
 }
